ed2ed2a4ac28fbde80c4bbc7e5114b4255f2c34b
VadimStrapko <vstrapko@internet.ru>
2025-03-15 17:59:15 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+
+
+
+	//2з
+	double a = 5.45, n = 5;
+	double y[]{ 1.3, 1, 0.9, 0.5, 8 };
+	for (int i = 0; i < 5; i++)
+	{
+		double q = 1;
+		for (int j = i; j <= n; j++)
+		{
+			double buff = 1;
+			buff = buff * j * j;
+			q = q * buff;
+
+		}
+		q *= y[i];
+		q = (q + min(y));
+
+		int s = 2 * a + q * sin(a);
+		cout << "q = " << q << " " << "s = " << s << endl;
+	}
+}
---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include<windows.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "ru");
+
+	int a;
+	unsigned char code;
+
+	cout << "Введите символ: ";
+	cin >> code;
+
+	cout << "Введите действие 1(англ.буквы), 2(рус.буквы), 3(цифры)" << endl;
+	cin >> a;
+
+	switch (a)
+	{
+	case 1:
+
+		if (code >= 'A' && code <= 'z')
+		{
+			cout << "Код Windows-125 заглавной буквы  " << " - " << int(code) << endl;
+			cout << "Код Windows-1251 строчной буквы  " << " - " << int(code - 32) << endl;
+			cout << "Разница - 32" << endl;
+		}
+		else
+		{
+			cout << "Ошибка" << endl;
+		}
+		break;
+
+	case 2:
+
+		if (int(code) > 128 && int(code) <= 240)
+		{
+			cout << "Код Windows-1251 заглавной буквы  " << " - " << int(code) << endl;
+			cout << "Код Windows-1251 строчной буквы " << " - " << int(code - 32) << endl;
+			cout << "Разница - 32" << endl;
+		}
+		else
+		{
+			cout << "Ошибка" << endl;
+		}
+		break;
+
+	case 3:
+
+		if (code >= '0' && code <= '9')
+		{
+			cout << "Код Windows-1251 " << " - " << int(code) << endl;
+		}
+		else
+		{
+			cout << "Ошибка" << endl;
+		}
+		break;
+
+	default:
+
+		cout << "Завершение программы" << endl;
+		exit(0);
+	}
+
+	return 0;
+
+}
